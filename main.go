66fd962d9409109b42cd56c6e502242225a8f4b0
Carter Peel <carter@gammabyte.xyz>
2021-05-12 14:28:09 -0400

    Started the createdomain API path & function

--- main.go
+++ main.go
@@ -35,6 +35,7 @@ func handleRequests() {
 	http.HandleFunc("/api/kvm/stats", getStats)
 	http.HandleFunc("/api/kvm/domains", getDomains)
 	http.HandleFunc("/api/kvm/ram-usage", getRamUsage)
+	http.HandleFunc("/api/kvm/create/domain", createDomain)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
@@ -88,4 +89,53 @@ func getRamUsage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(cmd)
 	fmt.Println(string(stdout))
 	fmt.Fprintf(w, string(stdout))
+}
+
+type createDomainStruct struct {
+	// VM Specs
+	RamSize         string
+	CpuSize         string
+	DiskSize        string
+	OperatingSystem string
+
+	// User Information
+	UserEmail string
+	UserID    string
+	FullName  string
+	UserRole  string
+
+	// Misc. Data
+	CreationDate string
+}
+
+func createDomain(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var t *createDomainStruct = &createDomainStruct{}
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+	err := decoder.Decode(&t)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	//for key, value := range r.Form {
+	//	fmt.Printf("%s = %s\n", key, value)
+	//	fmt.Fprintf(w, "%+v", t)
+	//}
+	//fmt.Fprintf(w, "%+v", t)
+	//fmt.Fprintf(w,"Create Domain:\n %+v", t)
+
+	fmt.Fprintf(w, "RAM => %sGB\n", t.RamSize)
+	fmt.Fprintf(w, "vCPUs => %s\n", t.CpuSize)
+	fmt.Fprintf(w, "Disk Size => %sGB\n", t.DiskSize)
+	fmt.Fprintf(w, "Operating System => %s\n", t.OperatingSystem)
+	fmt.Fprintf(w, "User Email => %s\n", t.UserEmail)
+	fmt.Fprintf(w, "User ID => %s\n", t.UserID)
+	fmt.Fprintf(w, "Full Name => %s\n", t.FullName)
+	fmt.Fprintf(w, "User Role => %s\n", t.UserRole)
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "VM Creation Date: %s\n", t.CreationDate)
+
 }
